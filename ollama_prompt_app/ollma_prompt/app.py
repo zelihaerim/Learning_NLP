7b017cd211b793321c0399daa618703f24e160ea
Zeliha Erim <zerim@gtu.edu.tr>
2024-11-14 16:14:51 +0300

    Add files via upload

--- ollama_prompt_app/ollma_prompt/app.py
+++ ollama_prompt_app/ollma_prompt/app.py
@@ -1,48 +1,3 @@
-"""from fastapi import FastAPI
-from langserve import add_routes
-import uvicorn
-from langchain_core.output_parsers import StrOutputParser
-# from langchain_community.llms import Ollama
-from langchain_ollama import OllamaLLM
-from langchain_community.chat_models import ChatOllama
-from langchain_core.prompts import ChatPromptTemplate
-
-
-
-llm = OllamaLLM(model="gemma2:2b")
-output_parser=StrOutputParser()
-
-prompt=ChatPromptTemplate.from_messages(
-    [
-        ("system","You are not an meteorologist, beside this you are a professional exper biologist. Give me answers based on your expertise."),
-        ("user","{question}")
-    ]
-)
-print(f"Prompt : {prompt}")
-## chain 
-chain=prompt|llm|output_parser
-response=chain.invoke({"question":"Can you tell me about altitude?"})
-print(f"Prompt : {prompt}")
-print(f"_."*75)
-print(f"{response}")
-
-
-app=FastAPI(
-    title="Langchain Server",
-    version="1.0",
-    decsription="A simple API Server"
-)
-
-add_routes(
-    app,
-    prompt|llm|output_parser,
-    path="/ollama"
-)
-
-if __name__=="__main__":
-    uvicorn.run(app,host="localhost",port=8000)
-"""
-
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langserve import add_routes
@@ -54,24 +9,21 @@ model= OllamaLLM(model="gemma2:2b")
 output_parser=StrOutputParser()
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are not an meteorologist, beside this you are a professional exper biologist. Give me answers based on your expertise."),
+        ("system","I am a professional expert computer scientist. Give me answers based on my expertise."),
         ("user","{question}")
     ]
 )
-
 
 app=FastAPI(
     title="Langchain Server",
     version="1.0",
     decsription="A simple API Server"
-
 )
 
 add_routes(
     app,
     prompt|model|output_parser,
     path="/ollama"
-
 )
 
 
--- ollama_prompt_app/ollma_prompt/client.py
+++ ollama_prompt_app/ollma_prompt/client.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
